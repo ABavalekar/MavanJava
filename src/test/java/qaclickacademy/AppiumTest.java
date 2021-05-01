012538cc3b83ae7c554fcbd9b6645fe92a97ea2a
ABavalekar <bavalekarankita27@gmail.com>
2021-05-02 00:21:21 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void MobileTest()
+	{
+		System.out.println("MobileTest");
+	}
+	
+	@Test
+	public void ioTest()
+	{
+		System.out.println("ioTest");
+	}
+	
+
+}
---
+++ src/test/java/qaclickacademy/RestAssuredTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+
+	@Test
+	public void APITest()
+	{
+		System.out.println("APITest");
+	}
+	
+	@Test
+	public void apiLinkTest()
+	{
+		System.out.println("apiLinkTest");
+	}
+
+
+
+}
